d4de202517b65fc7cd07858ef76d6889d914dce3
exaltpawarikanda <exaltpawa@gmail.com>
2021-10-05 18:53:42 +0200

    Added implementation to expose soap-ws

---
+++ exaltPawarikanda/src/main/java/za/co/discovery/assignment/ws/api/endpoint/InterstellarTransportEndpoint.java
@@ -0,0 +1,27 @@
+package za.co.discovery.assignment.ws.api.endpoint;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import za.co.discovery.assignment.schema.RoutePathRequest;
+import za.co.discovery.assignment.schema.RoutePathResponse;
+import za.co.discovery.assignment.ws.api.service.InterstellarWebService;
+
+/**
+ * @author Exalt Pawarikanda
+ */
+@RequiredArgsConstructor
+@Endpoint
+public class InterstellarTransportEndpoint {
+
+    private static final String NAMESPACE ="http://www.discovery.com/spring/soap/api/interstellarService";
+    private final InterstellarWebService interstellarWebService;
+
+    @PayloadRoot(namespace = NAMESPACE,localPart = "RoutePathRequest")
+    @ResponsePayload
+    public RoutePathResponse getShortestRoute(@RequestPayload RoutePathRequest request){
+        return interstellarWebService.findShortestRoute(request);
+    }
+}
---
+++ exaltPawarikanda/src/main/java/za/co/discovery/assignment/ws/api/service/InterstellarWebService.java
@@ -0,0 +1,28 @@
+package za.co.discovery.assignment.ws.api.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import za.co.discovery.assignment.schema.RoutePathRequest;
+import za.co.discovery.assignment.schema.RoutePathResponse;
+import za.co.discovery.assignment.util.ShortestPathService;
+
+import java.util.List;
+
+/**
+ * @author Exalt Pawarikanda
+ */
+@AllArgsConstructor
+@Service
+public class InterstellarWebService {
+
+    private final ShortestPathService shortestPathService;
+
+    public RoutePathResponse findShortestRoute(RoutePathRequest routePathRequest){
+        RoutePathResponse routePathResponse = new RoutePathResponse();
+        List<String> path = shortestPathService.initializeGraph(routePathRequest.getStartPlanet(),routePathRequest.getDestinationPlanet(),routePathRequest.isIsTraffic());
+        routePathResponse.setStartPlanet(routePathRequest.getStartPlanet());
+        routePathResponse.setDestinationPlanet(routePathRequest.getDestinationPlanet());
+        path.stream().forEach(p-> routePathResponse.getRoute().add(p));
+        return routePathResponse;
+    }
+}
